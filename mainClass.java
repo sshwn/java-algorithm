6b30b6dd6cd2ef0336f3a20e5018c16dd95abc37
chaewon <woon1998@naver.com>
2024-09-06 20:39:03 +0900

    [메인 클래스]

---
+++ mainClass.java
@@ -0,0 +1,8 @@
+import Chaewon.Chapter7.ArrayPartition1;
+
+public class mainClass {
+    public static void main(String[] args) {
+        ArrayPartition1 arrayPartition1 = new ArrayPartition1();
+        System.out.print(arrayPartition1.myAnswer());
+    }
+}
