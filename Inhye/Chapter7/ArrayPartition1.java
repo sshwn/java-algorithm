99f2a3cb7bb7735a0a5402e4c7af317b812f70f0
inhyeheo <inhyeheo@naonsoft.com>
2024-09-06 20:32:54 +0900

    디렉토리 생성

---
+++ Inhye/Chapter7/ArrayPartition1.java
@@ -0,0 +1,14 @@
+package Chaewon.Chapter7;
+
+import java.util.Arrays;
+public class ArrayPartition1 {
+
+    /**
+     * 처음엔 정렬하고 인덱스 0에서부터 최솟값을 찾도록 했는데 그러면 input의 갯수가 홀수일 때 answer의 최댓값을 찾지 못해서
+     * 맨 끝부터 최솟값을 찾는 걸로 변경
+     */
+    public int myAnswer(){
+
+    }
+
+}
