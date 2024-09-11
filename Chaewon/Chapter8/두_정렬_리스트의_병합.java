26a85abf2514c17f4a0c5547f4aa0cd9edb65a1d
chaewon <woon1998@naver.com>
2024-09-11 21:12:21 +0900

    [chapter8] 두 정렬 리스트의 병합 ListNode로 문제풀이

--- Chaewon/Chapter8/두_정렬_리스트의_병합.java
+++ Chaewon/Chapter8/두_정렬_리스트의_병합.java
@@ -3,6 +3,29 @@ import java.util.LinkedList;
 import java.util.Collections;
 
 public class 두_정렬_리스트의_병합 {
+    public ListNode<Integer> myAnswerUseListNode(){
+        int[] data1 = {1, 2, 5};
+        ListNode<Integer> node1 = new ListNode<>();
+        node1 = node1.addIntData(data1);
+
+        int[] data2 = {1, 3, 4};
+        ListNode<Integer> node2 = new ListNode<>();
+        node2 = node2.addIntData(data2);
+
+        ListNode<Integer> answer = new ListNode();
+
+        while(node1 != null && node2 != null){
+            if(node1.val > node2.val){
+                answer = node1;
+                answer.next = node2;
+            }else{
+                answer = node2;
+                answer.next = node1;
+            }
+        }
+        return node2;
+    }
+
     public LinkedList myAnswer(){
         LinkedList<Integer> input1 = new LinkedList<>();
         input1.add(1);
