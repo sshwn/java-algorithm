7b9ef28272288d47a7956598578298de49e4cdb4
chaewon <woon1998@naver.com>
2024-09-11 21:10:16 +0900

    [chapter8] ListNode 클래스 재정의

--- Chaewon/Chapter8/ListNode.java
+++ Chaewon/Chapter8/ListNode.java
@@ -1,9 +1,32 @@
 package Chaewon.Chapter8;
 
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+public class ListNode<E> {
+    public E val;
+    public ListNode<E> next;
+
+    public ListNode() {}
+
+    public ListNode(E value){
+        this.val = value;
+    }
+
+    public ListNode(E value, ListNode<E> next){
+        this.val = value;
+        this.next = next;
+    }
+
+    /**
+     * int 배열로 받은 문제의 input 데이터를 ListNode에 넣음
+     * @param input
+     * @return
+     */
+    public ListNode<Integer> addIntData(int[] input){
+        ListNode<Integer> head = new ListNode(0);
+        ListNode<Integer> last = head;
+        for(int i = 0; i < input.length; i++){
+            last.next = new ListNode(input[i]);
+            last = last.next;
+        }
+        return head.next;
+    }
 }
